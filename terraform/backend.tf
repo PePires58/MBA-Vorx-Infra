fdd751b80503e4020f3d356c67dd8352d70b7d5f
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-03-07 18:15:04 -0300

    updating development workflow

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "vorx-tf-artifacts"
-    key    = ""
-    region = "east-us-1"
-  }
-}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.54.0"
+      version = "~>4.54.0"
     }
+  }
+
+  required_version = ">=0.14.9"
+
+  backend "s3" {
+    bucket = "vorx-tf-artifacts"
+    key    = ""
+    region = "east-us-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  version = "~>3.0"
 }
