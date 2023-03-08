98dab42acef4020fd41b66e5ed9da1fa2d94b6a5
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-03-07 22:12:55 -0300

    trying module configuration

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,106 +1,18 @@
-resource "aws_vpc" "vorx-vpc-prod" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Vorx-vpc-PROD"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.10.0/24", "10.0.20.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = true
 
   tags = {
-    Name = "Vorx-vpc-PROD"
+    Name        = "Vorx-vpc"
+    Environment = "prod"
   }
-}
-
-resource "aws_subnet" "vorx-subnet-public-1a" {
-  vpc_id            = aws_vpc.vorx-vpc-prod.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "prod-vorx-subnet-publica-1a"
-  }
-}
-
-resource "aws_subnet" "vorx-subnet-public-1b" {
-  vpc_id            = aws_vpc.vorx-vpc-prod.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "prod-vorx-subnet-publica-1b"
-  }
-}
-
-
-resource "aws_subnet" "vorx-subnet-private-1a" {
-  vpc_id            = aws_vpc.vorx-vpc-prod.id
-  cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "prod-vorx-subnet-private-1a"
-  }
-}
-
-resource "aws_subnet" "vorx-subnet-private-1b" {
-  vpc_id            = aws_vpc.vorx-vpc-prod.id
-  cidr_block        = "10.0.20.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "prod-vorx-subnet-private-1b"
-  }
-}
-
-resource "aws_internet_gateway" "vorx-vpc-prod-internet-gateway" {
-  vpc_id = aws_vpc.vorx-vpc-prod.id
-
-  tags = {
-    Name = "prod-vorx-vpc-internet-gateway"
-  }
-}
-
-resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.vorx-vpc-prod.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.vorx-vpc-prod-internet-gateway.id
-  }
-
-  tags = {
-    Name = "prod-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "association-vorx-subnet-public-1a" {
-  subnet_id      = aws_subnet.vorx-subnet-public-1a.id
-  route_table_id = aws_route_table.public-rt.id
-}
-
-resource "aws_route_table_association" "association-vorx-subnet-public-1b" {
-  subnet_id      = aws_subnet.vorx-subnet-public-1b.id
-  route_table_id = aws_route_table.public-rt.id
-}
-
-
-resource "aws_route_table" "vorx-private-rt-1a" {
-  vpc_id = aws_vpc.vorx-vpc-prod.id
-
-  tags = {
-    Name = "prod-private-rt-1a"
-  }
-}
-
-resource "aws_route_table" "vorx-private-rt-1b" {
-  vpc_id = aws_vpc.vorx-vpc-prod.id
-
-  tags = {
-    Name = "prod-private-rt-1b"
-  }
-}
-
-resource "aws_route_table_association" "association-vorx-private-rt-1a" {
-  subnet_id      = aws_subnet.vorx-subnet-private-1a.id
-  route_table_id = aws_route_table.vorx-private-rt-1a.id
-}
-
-resource "aws_route_table_association" "association-vorx-private-rt-1b" {
-  subnet_id      = aws_subnet.vorx-subnet-private-1b.id
-  route_table_id = aws_route_table.vorx-private-rt-1b.id
 }
